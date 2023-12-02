fd2bed63e279d39e74dbae2df23706e7aace05e1
Davide <24529587+ubertidavide@users.noreply.github.com>
2023-12-02 16:46:23 +0100

    upgraded

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import argparse
+import logging
+
 from tasks.test_task import TestTask
 
-        
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='MyBot',
+        description='Bot builded with the fastbots library.',
+        epilog='Made with <3 using fastbots.')
+
+    parser.add_argument('-tt', '--test_task', action='store_true')
+    
+    args = parser.parse_args()
+
+    if args.test_task:
+        logging.info('Launching Test Task')
+        TestTask()()
+
+
 if __name__ == '__main__':
-    # start the task
+    #main()
     TestTask()()
